c8ea64bc760ac2eebc81c010342900e6a0f4bedc
Colack <UniformKitten281@gmail.com>
2024-01-16 09:29:20 -0800

    Commit
    Repository Time

---
+++ common.h
@@ -0,0 +1,30 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
+#include <sys/time.h>
+#include <Windows.h>
+
+#define COM_WATERMELONKATANA_VERSION "0.0.1"
+#define COM_WATERMELONKATANA_VERSION_MAJOR 0
+#define COM_WATERMELONKATANA_VERSION_MINOR 0
+#define COM_WATERMELONKATANA_VERSION_PATCH 1
+
+#define COM_WATERMELONKATANA_REPOSITORY "https://github.com/colack/rx-178-mkii"
+#define COM_WATERMELONKATANA_WEBSITE "https://watermelonkatana.com/projects/rx-178-mkii"
+
+#define COM_WATERMELONKATANA_AUTHOR "colack"
+#define COM_WATERMELONKATANA_AUTHOR_EMAIL "colackalpha@gmail.com"
+#define COM_WATERMELONKATANA_AUTHOR_WEBSITE "https://watermelonkatana.com"
+
+#define COM_WATERMELONKATANA_LICENSE "MIT License"
+
+#define COM_WATERMELONKATANA_NAME "RX-178 MKII"
+#define COM_WATERMELONKATANA_BIO "Doom-Styled space shooter game"
+#define COM_WATERMELONKATANA_DESCRIPTION "RX-178 MKII is a Doom-Styled space shooter game. It is a 2D game with 3D graphics. The game is written in C++ and uses OpenGL for graphics rendering. The game is inspired by the game 'R-Type' and the anime 'Mobile Suit Zeta Gundam'."
+
+#endif
---
+++ game/video.h
@@ -0,0 +1,11 @@
+#ifndef VIDEO_H
+#define VIDEO_H
+
+#include "rx-types.h"
+#include "rx-data.h"
+#include "rx-def.h"
+
+#define VIDEO_WINDOW_TITLE "RX-178 MKII"
+#define VIDEO_CENTER (SCREENHEIGHT / 2)
+
+extern 
